6e738066cf4763105692b4f6a386d622dc80e495
Daniel Waghorne <dwaghorne@hotmail.com>
2024-01-29 11:38:07 +1000

    Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'Name': `${name}`,
+            'Email': `${email}`
+        })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+}
+
+submitData("Daniel Waghorne", "dwaghorne@hotmail.com")
